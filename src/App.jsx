f7d41dec49f4746ec5c8f6c6626978953fe1b677
samuelbahiru <samuelbahiru93@gmail.com>
2024-02-26 10:26:44 +0300

    UseQuery basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const POSTS = [
+    {
+      id: "1",
+      title: "Post 1",
+    },
+    {
+      id: "2",
+      title: "Post 2",
+    },
+  ];
+
+  const postsQuery = useQuery({
+    queryKey: ["getting posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  if (postsQuery.isLoading) {
+    return <h1>is Loading...</h1>;
+  }
+  if (postsQuery.error) {
+    return <pre>{JSON.stringify(error)}</pre>;
+  }
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <div>
+        {postsQuery.data.map((post) => {
+          return <h1 key={post.id}>{post.title}</h1>;
+        })}
+      </div>
     </>
   );
+}
+
+function wait(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = QueryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
